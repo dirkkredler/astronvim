b678ca3abd7af66c3ef97500e7865e745cf44db7
Dirk Kredler <dirk.kredler@gmail.com>
2024-04-20 19:12:42 +0200

    chore(settings): add dedicated settings file, along with some core vim settings

---
+++ lua/plugins/settings.lua
@@ -0,0 +1,21 @@
+vim.cmd [[
+    set guicursor=n-v-c:block,i-ci-ve:hor20blinkon250,r-cr:ver20blinkon250,o:hor50
+    \,sm:block-blinkwait175-blinkoff150-blinkon175
+]]
+
+---@type LazySpec
+return {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+        options = {
+            opt = {
+                shiftwidth = 4,
+                tabstop = 4,
+                wrap = true,
+                spelllang = { "en", "de" },
+                spell = true,
+            },
+        },
+    },
+}
