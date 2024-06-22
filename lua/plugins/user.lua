10110068e325f5e1d94948b659e1354b3d041c9a
Dirk Kredler <dirk.kredler@gmail.com>
2024-06-22 14:41:33 +0200

    feat(chatgpt): add additional package, see https://github.com/Robitx/gp.nvim/

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,85 +1,119 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
-
----@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
-
-  -- customize alpha options
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- customize the dashboard header
-      opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
-      }
-      return opts
-    end,
-  },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
+  "robitx/gp.nvim",
+  config = function()
+    require("gp").setup {
+      chat_topic_gen_model = "gpt-4o",
+      agents = {
+        -- Disable ChatGPT 3.5
         {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          name = "ChatGPT3-5",
+          chat = false, -- just name would suffice
+          command = false, -- just name would suffice
         },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
+        {
+          name = "ChatGPT4",
+          chat = true,
+          command = true,
+          -- string with model name or table with model name and parameters
+          model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+          -- system prompt (use this to specify the persona/role of the AI)
+          system_prompt = "You are a general AI assistant.\n\n"
+            .. "The user provided the additional info about how they would like you to respond:\n\n"
+            .. "- If you're unsure don't guess and say you don't know instead.\n"
+            .. "- Ask question if you need clarification to provide better answer.\n"
+            .. "- Think deeply and carefully from first principles step by step.\n"
+            .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+            .. "- Use Socratic method to improve your thinking and coding skills.\n"
+            .. "- Don't elide any code from your output if the answer requires coding.\n"
+            .. "- Take a deep breath; You've got this!\n",
+        },
+      },
+    }
+  end,
 }
+
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--
+-- -- You can also add or configure plugins by creating files in this `plugins/` folder
+-- -- Here are some examples:
+--
+-- ---@type LazySpec
+-- return {
+--
+--   -- == Examples of Adding Plugins ==
+--
+--   "andweeb/presence.nvim",
+--   {
+--     "ray-x/lsp_signature.nvim",
+--     event = "BufRead",
+--     config = function() require("lsp_signature").setup() end,
+--   },
+--
+--   -- == Examples of Overriding Plugins ==
+--
+--   -- customize alpha options
+--   {
+--     "goolord/alpha-nvim",
+--     opts = function(_, opts)
+--       -- customize the dashboard header
+--       opts.section.header.val = {
+--         " █████  ███████ ████████ ██████   ██████",
+--         "██   ██ ██         ██    ██   ██ ██    ██",
+--         "███████ ███████    ██    ██████  ██    ██",
+--         "██   ██      ██    ██    ██   ██ ██    ██",
+--         "██   ██ ███████    ██    ██   ██  ██████",
+--         " ",
+--         "    ███    ██ ██    ██ ██ ███    ███",
+--         "    ████   ██ ██    ██ ██ ████  ████",
+--         "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+--         "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+--         "    ██   ████   ████   ██ ██      ██",
+--       }
+--       return opts
+--     end,
+--   },
+--
+--   -- You can disable default plugins as follows:
+--   { "max397574/better-escape.nvim", enabled = false },
+--
+--   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+--   {
+--     "L3MON4D3/LuaSnip",
+--     config = function(plugin, opts)
+--       require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+--       -- add more custom luasnip configuration such as filetype extend or custom snippets
+--       local luasnip = require "luasnip"
+--       luasnip.filetype_extend("javascript", { "javascriptreact" })
+--     end,
+--   },
+--
+--   {
+--     "windwp/nvim-autopairs",
+--     config = function(plugin, opts)
+--       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+--       -- add more custom autopairs configuration such as custom rules
+--       local npairs = require "nvim-autopairs"
+--       local Rule = require "nvim-autopairs.rule"
+--       local cond = require "nvim-autopairs.conds"
+--       npairs.add_rules(
+--         {
+--           Rule("$", "$", { "tex", "latex" })
+--             -- don't add a pair if the next character is %
+--             :with_pair(cond.not_after_regex "%%")
+--             -- don't add a pair if  the previous character is xxx
+--             :with_pair(
+--               cond.not_before_regex("xxx", 3)
+--             )
+--             -- don't move right when repeat character
+--             :with_move(cond.none())
+--             -- don't delete if the next character is xx
+--             :with_del(cond.not_after_regex "xx")
+--             -- disable adding a newline when you press <cr>
+--             :with_cr(cond.none()),
+--         },
+--         -- disable for .vim files, but it work for another filetypes
+--         Rule("a", "a", "-vim")
+--       )
+--     end,
+--   },
+-- }
